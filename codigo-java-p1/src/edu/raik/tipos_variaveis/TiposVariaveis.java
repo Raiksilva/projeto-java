78921c937cb7852814ad951355684e4e13915320
raiksilva <raiksilva2016@gmail.com>
2023-01-04 13:56:55 -0300

    Continuando os estudos!

---
+++ codigo-java-p1/src/edu/raik/tipos_variaveis/TiposVariaveis.java
@@ -0,0 +1,21 @@
+package edu.raik.tipos_variaveis; 
+public class TiposVariaveis{
+    public static void main(String[] args) {
+       
+       
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        
+        numero = 9;
+        System.out.println(numero);
+        System.out.println(numeroCurto2);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+    }
+}
